3106a05f5ddb623f843db8e7a7998d3c2213afc9
NebulosBit <127682659+NebulosBit@users.noreply.github.com>
2023-04-23 19:03:16 +0000

    Added screen UI with 500px box with 16 grid square each

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+const screen = document.querySelector(".screen")
+
+function createGrid(size) {
+  screen.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  screen.innerHTML = "";
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square")
+    screen.appendChild(square);
+  }
+}
+
+grid = 16
+createGrid(grid);
